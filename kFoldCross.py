ef55862da09fa3bdfea1e1373d122f093aeb7819
Daniel <drsaltiel@gmail.com>
2014-11-19 20:19:19 -0800

    changed how crossValidate function holds scores

--- kFoldCross.py
+++ kFoldCross.py
@@ -12,14 +12,14 @@ def crossValidate(X, y, classifier, k_folds) :
     indicies = KFold(len(X), n_folds=k_folds, shuffle = True)
     
     #get scores
-    scores = []
+    scores = 0
     for train, test in indicies:
-        knn = classifier.fit(X[train],y[train])
-        score = knn.score(X[test], y[test])
-        scores.append(score)
+        model = classifier.fit(X[train],y[train])
+        score = model.score(X[test], y[test])
+        scores+=score
 
     # return the average accuracy
-    return sum(scores)/float(k_folds)
+    return scores/float(k_folds)
 
 
 
@@ -29,34 +29,34 @@ def crossValidate(X, y, classifier, k_folds) :
 
 import random
 
-def kFoldCross(k, algorithm, data, random_seed = None):
-    '''
-    implimentation of k-fold cross validation
-    inputs: k - number of folds 
-            algorithm - classification algorithm
-            data - data for classification
-            random_seed: seed for initial division
-    outputs: average test error for all folds
-    '''
-    #initial divide data
-    random.seed = random_seed
-    indicies = random.shuffle(range(0,len(data)))
-    test_i = len(data)/k
+# def kFoldCross(k, algorithm, data, random_seed = None):
+#     '''
+#     implimentation of k-fold cross validation
+#     inputs: k - number of folds 
+#             algorithm - classification algorithm
+#             data - data for classification
+#             random_seed: seed for initial division
+#     outputs: average test error for all folds
+#     '''
+#     #initial divide data
+#     random.seed = random_seed
+#     indicies = random.shuffle(range(0,len(data)))
+#     test_i = len(data)/k
 
-    #test error on each train/test divide
-    errors = [0]*k
-    for i in range(1, k+1):
-        train = data[(i-1)*test_i:i*test_i]
-        test = data[:(i-1)*test_i]
-        test.append(data[i*_i:])
+#     #test error on each train/test divide
+#     errors = [0]*k
+#     for i in range(1, k+1):
+#         train = data[(i-1)*test_i:i*test_i]
+#         test = data[:(i-1)*test_i]
+#         test.append(data[i*_i:])
 
-        #train on data
-        knn = classifier.fit(X[train],y[train])
-        error = knn.score(X[test], y[test])
+#         #train on data
+#         knn = classifier.fit(X[train],y[train])
+#         error = knn.score(X[test], y[test])
 
-        #test error
-        errors[i] = error
+#         #test error
+#         errors[i] = error
 
-    #average error and output
-    ave_error = sum(errors)/float(k)
-    return ave_error
+#     #average error and output
+#     ave_error = sum(errors)/float(k)
+#     return ave_error
